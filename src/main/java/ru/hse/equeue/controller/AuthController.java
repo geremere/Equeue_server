3099b6861a1e55a47ab0837eac4fa18d026107c0
rmimamov <rad.imamow@yandex.ru>
2022-05-08 19:31:07 +0300

    cors

--- src/main/java/ru/hse/equeue/controller/AuthController.java
+++ src/main/java/ru/hse/equeue/controller/AuthController.java
@@ -2,6 +2,7 @@ package ru.hse.equeue.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ public class AuthController {
     private final AuthService authService;
 
     @GetMapping(EndPoints.AUTH)
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
     public String auth(@PathVariable("token") String idToken){
         return authService.auth(idToken);
     }
