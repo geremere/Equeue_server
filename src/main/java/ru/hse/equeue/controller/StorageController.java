784f0c42b46be41ae1debadc6a4214fd1dd0e4e0
rmimamov <rad.imamow@yandex.ru>
2022-05-10 15:56:33 +0300

    add cross origin to image

--- src/main/java/ru/hse/equeue/controller/StorageController.java
+++ src/main/java/ru/hse/equeue/controller/StorageController.java
@@ -3,6 +3,7 @@ package ru.hse.equeue.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ public class StorageController {
     private final AmazonAwsS3Client s3Client;
 
     @GetMapping(EndPoints.GET_IMAGE_BY_NAME)
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
     public ResponseEntity<ByteArrayResource> getImage(@PathVariable String imageName) {
         byte[] data = s3Client.getFile(imageName);
         ByteArrayResource resource = new ByteArrayResource(data);
